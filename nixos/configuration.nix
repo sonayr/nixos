ce7dc0ee6742c96bd44218b7655734f8de1fb99c
Ryan <ryano@Ryans-MacBook-Pro.local>
2025-05-12 15:25:32 -0300

    add kanata to config imports

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.home-manager
+      ../services/kanata.nix
     ];
 
   # Bootloader.
