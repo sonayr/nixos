73a8ba93ea5af57aeeed1be1f710f896e746501b
Ryan O'Sullivan <ryanosullivan360@gmail.com>
2026-07-08 08:12:49 -0400

    chore: fix home assistant http config

--- services/system/home-assistant.nix
+++ services/system/home-assistant.nix
@@ -13,10 +13,6 @@
     ];
     config = {
       default_config = {};
-      http = {
-        server_host = [ "0.0.0.0" ];
-        server_port = 8123;
-      };
     };
   };
   networking.firewall.allowedTCPPorts = [ 8123 ];
