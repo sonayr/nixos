3257dcec340ec5372ba354d596d889d68a6b6a85
Ryan O'Sullivan <ryanosullivan360@gmail.com>
2026-07-22 20:26:04 -0400

    fix(laptop): remove deprecated displayManager.gdm.wayland

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -38,7 +38,6 @@
   # Enable the GNOME Desktop Environment.
   services.displayManager.gdm = {
     enable = true;
-    wayland = true;
   };
 
   # services.xserver.desktopManager.gnome.enable = true;
