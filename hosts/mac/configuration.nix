ceab4f10cca1859b443f48365b434a386d6fb2e1
Ryan O'Sullivan <ryanosullivan360@gmail.com>
2026-07-23 18:59:38 -0400

    fix(mac): add video and input groups to user for wayland

--- hosts/mac/configuration.nix
+++ hosts/mac/configuration.nix
@@ -74,7 +74,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ryan = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "video" "audio" "input" "networkmanager" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       tree
     ];
